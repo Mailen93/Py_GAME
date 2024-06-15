8716d02bd7e9983cf9e8b26eaaa1c6d3333eae86
Mailen Lorena Alvarez <m.labarrere2@gmail.com>
2024-06-15 01:04:23 -0300

    Adds ellipse, circle and triangle

--- Figuras/Demo.py
+++ Figuras/Demo.py
@@ -15,11 +15,26 @@ pygame.display.set_caption("Mi primer juego")
 
 PANTALLA.fill(BLANCO)
 
-pygame.draw.line(PANTALLA, VERDE, (100,100), (350,100), 5) #? X1,Y1 - X2, Y2 Linea horizontal
-pygame.draw.line(PANTALLA, AZUL, (100,100), (350,250), 5) # Linea decreciente
-pygame.draw.line(PANTALLA, ROJO, (200,200), (350,100), 5) # Linea creciente 
-pygame.draw.line(PANTALLA, NEGRO, (100,200), (100,300), 5) # Linea vertical
+# ! LINEA:
+# pygame.draw.line(PANTALLA, VERDE, (100,100), (350,100), 5) #? X1,Y1 - X2, Y2 Linea horizontal
+# pygame.draw.line(PANTALLA, AZUL, (100,100), (350,250), 5) # Linea decreciente
+# pygame.draw.line(PANTALLA, ROJO, (200,200), (350,100), 5) # Linea creciente 
+# pygame.draw.line(PANTALLA, NEGRO, (100,200), (100,300), 5) # Linea vertical
 
+# ! CUADRADO - RECTÁNGULO:
+pygame.draw.rect(PANTALLA, VERDE, (50,50,100,100)) #? X,Y,ANCHO,ALTO
+pygame.draw.rect(PANTALLA, ROJO, (200,90,100,300))
+
+# ! CÍRCULO:
+pygame.draw.circle(PANTALLA, AZUL, (60,60), 50, 2) #? EPICENTRO - RADIO - GROSOR DE LA LÍNEA
+
+# ! ELIPSE:
+pygame.draw.ellipse(PANTALLA, NEGRO, (50,50,200,100),1)
+pygame.draw.ellipse(PANTALLA, NEGRO, (250,100,100,300),1)
+
+# ! TRÍANGULO:
+puntos = [(100,300), (100,100), (150,100)]
+pygame.draw.polygon(PANTALLA, AZUL, puntos)
 
 flag = True
 while flag == True: #Bucle infinito
